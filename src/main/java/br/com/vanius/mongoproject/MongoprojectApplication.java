313b2a407a668ba122c097a6015b989a2eb2d0cd
vaniusmax <vaniusmax@gmail.com>
2018-08-07 21:42:46 -0300

    Primeiro commit

---
+++ src/main/java/br/com/vanius/mongoproject/MongoprojectApplication.java
@@ -0,0 +1,12 @@
+package br.com.vanius.mongoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoprojectApplication.class, args);
+	}
+}
